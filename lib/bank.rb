80ffccc52abd3fc14f4aa84391a69bb7cc6d7a48
dean <d.lewis.rave@gmail.com>
2022-01-04 17:12:31 +0000

    withdraw from account and test

--- lib/bank.rb
+++ lib/bank.rb
@@ -11,4 +11,8 @@ class Bank
     @balance += amount
   end
 
+  def withdraw(amount)
+    @balance -= amount
+  end
+
 end
--- spec/bank_spec.rb
+++ spec/bank_spec.rb
@@ -12,4 +12,13 @@ describe Bank do
       expect { account.deposit(10) }.to change { account.balance }.by 10
     end
   end
+  
+  describe '#withdraw' do
+    account = Bank.new
+    account.deposit(10)
+    it { is_expected.to respond_to(:withdraw).with(1).argument }
+    it 'will withdraw account with 10' do
+      expect { account.withdraw(10) }.to change { account.balance }.by -10
+    end
+  end
 end
