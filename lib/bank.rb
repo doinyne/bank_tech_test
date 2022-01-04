5e2aaa055bb525d61ce34d187e0fc64ccebb54b1
dean <d.lewis.rave@gmail.com>
2022-01-04 18:00:12 +0000

    added timestap to deposits, withdraws and statements

--- lib/bank.rb
+++ lib/bank.rb
@@ -1,21 +1,25 @@
-# might need to require something later
+require 'date'
 
 class Bank
-  attr_reader :balance
+  attr_reader :balance, :statment
 
   def initialize
     @balance = 0
+    @statement = []
   end
 
   def deposit(amount)
     @balance += amount
+    @statement << { date: Date.today, balance: @balance }
   end
 
   def withdraw(amount)
     @balance -= amount
+    @statement << { date: Date.today, balance: @balance }
   end
 
   def statement
-    @balance
+    puts 'date || balance'
+    @statement.reverse.each { |transaction| puts "#{transaction[:date]} || #{transaction[:balance]}" }
   end
 end
